e7e286f37c6f537c27ff68c28949d67dfeeb03cb
marinacwebber <105221777+marinacwebber@users.noreply.github.com>
2023-09-02 17:58:34 -0300

    Correção dos testes com falhas

--- SitePrismCucumber/features/page_objects/home.rb
+++
@@ -1,7 +0,0 @@
-#language: pt
-
-class Home < SitePrism::Page
-    set_url '/'
-
-    element :logo, 'img[class="lnXdpd"]'  #página com mapeamento do logo. Esta validção é mais simples
-end
--- SitePrismCucumber/features/support/hooks.rb
+++ SitePrismCucumber/features/support/hooks.rb
@@ -6,9 +6,6 @@ end
 # permite que você veja exatamente o que aconteceu durante a execução do teste quando ocorreu uma falha
 After do |scenario|
     puts 'Estou executando depois'
-    puts scenario.failed?
+   #puts scenario.failed?
 end
 
-After('@limpar_banco') do
- puts 'EXECUTANDO DEPOIS DO CENÁRIO Carregar página do Google CENÁRIO 2'
-end
